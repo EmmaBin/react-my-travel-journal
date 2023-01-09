f62e0e232b16bf5394f559d216ae6466447747c9
emmacodes <binma.emma@gmail.com>
2023-01-09 10:59:02 -0800

    added data

---
+++ src/data.jsx
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        
+        startDate: "12 Apr, 2015",
+        endDate: "14 Apr, 2015",
+        description: "I flew to Japan once as a flight attendant, Japan was one of our favorites in our industry. The reason why we love it so much for several facts: 1, Passengers are nice and friendly. I vividly recalled when we landed, all passengers folded their blanks nicely as if no one were sitting there. 2, Food and shopping, even room service is so much better than other places.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Amsterdam",
+        location: "Netherlands",
+        
+        startDate: "7 Aug, 2014",
+        endDate: "10 Aug, 2014",
+        description: "Amsterdam was my first European station. I have known this place from the fairytale story. When I was sightseeing this city, it did look like a birthplace for so many moving stories. I remembered after settling down myself in hotel, I rushed outside to find the commute to the village famous for windmill despite the fact that I haven't slept for almost 24 hrs.",
+        imageUrl: "https://images.unsplash.com/photo-1584003564911-a7a321c84e1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1284&q=80"
+    },
+    {
+        title: "Iceland",
+        location: "Iceland",
+       
+        startDate: "21 Jul, 2018",
+        endDate: "30 Jul, 2018",
+        description: "I went to Iceland while I was studying in the U.S. Every scene is breath-taking, the mountains, the glacier, the icelandic horses, and even the gloomy weather suits the natural landscape so well.",
+        imageUrl: "https://images.unsplash.com/photo-1498008122250-bcb854c8462d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+    }
+]
