93c61afe6215630ce530f72c320ef41a9b11bcb1
emmacodes <binma.emma@gmail.com>
2023-01-09 11:35:54 -0800

    mapped objects to display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import pin from '../assets/pin.png'
 
-export default function Card(){
+export default function Card(props){
     return(
-        
+        <>
+        <div className='card-list'>
+            <div className='card'>
+                <img src={props.item.imageUrl} className='card--img' />
+                <img src={pin} />
+                <p className='card--location'>{props.item.location}</p>
+                <h1>{props.item.title}</h1>
+                <h3>{props.item.startDate} - {props.item.endDate}</h3>
+                <p className='card--text'>{props.item.description}</p>
+
+            </div>
+        </div>
+        </>
     )
 }
+
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 export default function Nav(){
-    
+    return(
+        <nav className='nav'>
+            <img src={logo} className='logo' />
+            <p>my travel journal.</p>
+        </nav>
+
+    )
+
 }
